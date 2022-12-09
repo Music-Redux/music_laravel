7fb62f7f2bd43dae0b8896ea7b8d467a9cc8f73d
nourman23 <nourmanzawahreh@gmail.com>
2022-12-09 15:50:59 +0300

    database

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('first-name');
+            $table->string('last-name');
+            $table->binary('avatar');
+            $table->boolean('role');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->timestamp('email_verified_at')->nullable();
+            $table->rememberToken();
+            $table->timestamps();
+        });
+        DB::statement(" ALTER TABLE users CHANGE `avatar` `avatar` MEDIUMBLOB NULL DEFAULT NULL");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
